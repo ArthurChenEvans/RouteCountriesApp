14cc4e1b4e87fb8170c4c2d51a4763c8c06d84a9
Arthur Evans <arthur.evans@pdms.com>
2024-02-26 15:12:58 +0000

    Created routing to static files

--- RouteCountriesApp/RouteCountriesApp/Program.cs
+++ RouteCountriesApp/RouteCountriesApp/Program.cs
@@ -2,10 +2,37 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
+    endpoints.MapGet("/", () => "1. USA \n2. Canada \n3. UK \n4. India \n5. Japan");
     endpoints.MapGet("/countries", () => "1. USA \n2. Canada \n3. UK \n4. India \n5. Japan");
-    endpoints.MapGet("/countries/{CountryID:int}", 
+    endpoints.MapGet("/countries/{CountryID:int:range(1,100)}", async context =>
+    {
+        if (context.Request.RouteValues.TryGetValue("CountryID", out var countryIDValue) && int.TryParse(countryIDValue?.ToString(), out int countryID))
+        {
+            var filePath = Path.Combine(app.Environment.WebRootPath, $"{countryID}.html");
+
+            if (File.Exists(filePath))
+            {
+                context.Response.ContentType = "text/html";
+                await context.Response.SendFileAsync(filePath);
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("Country not found");
+            }
+        }
+        else
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync("Country ID must be an integer");
+        }
+       
+    });
 });
 app.Run();
--- RouteCountriesApp/RouteCountriesApp/obj/Debug/net8.0/RouteCountriesApp.AssemblyInfo.cs
+++ RouteCountriesApp/RouteCountriesApp/obj/Debug/net8.0/RouteCountriesApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RouteCountriesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aabf67c7271d76c5549485c4ca9185133a86f065")]
 [assembly: System.Reflection.AssemblyProductAttribute("RouteCountriesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RouteCountriesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
