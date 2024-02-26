a867f38c380df21343068514942692961c894394
Arthur Evans <arthur.evans@pdms.com>
2024-02-26 14:37:23 +0000

    Setting up mapget methods

--- RouteCountriesApp/RouteCountriesApp/Program.cs
+++ RouteCountriesApp/RouteCountriesApp/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/countries", () => "1. USA \n2. Canada \n3. UK \n4. India \n5. Japan");
+    endpoints.MapGet("/countries/{CountryID:int}", 
+});
 app.Run();
